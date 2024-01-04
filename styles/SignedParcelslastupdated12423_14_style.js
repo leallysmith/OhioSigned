ed435469dd1213173cb7d453274738c9e169d37f
leallysmith <89225401+leallysmith@users.noreply.github.com>
2024-01-04 11:38:31 -0700

    new nmtc layer

--- styles/SignedParcelslastupdated12423_14_style.js
+++ styles/SignedParcelslastupdated12423_14_style.js
@@ -8,31 +8,20 @@ var style_SignedParcelslastupdated12423_14 = function(feature, resolution){
     };
     var value = ""
     var labelText = "";
-    var clusteredFeatures = feature.get("features");
-    var labelFont = "10.4px \'Open Sans\', sans-serif";
-    var labelFill = "#323232";
+    size = 0;
+    var labelFont = "10px, sans-serif";
+    var labelFill = "#000000";
     var bufferColor = "";
     var bufferWidth = 0;
-    size = clusteredFeatures.length;
-    var textAlign = "center";
-    var offsetX = 0;
-    var offsetY = 0;
-    if (size == 1) {
-        textAlign = "left"
-        offsetX = 8
-        offsetY = 3
-        var feature = clusteredFeatures[0];
-        if ("" !== null) {
-            labelText = String("");
-        }
-        key = value + "_" + labelText
-    } else {
-        labelText = size.toString()
-        size = 2*(Math.log(size)/ Math.log(2))
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'point';
+    if ("" !== null) {
+        labelText = String("");
     }
     var style = [ new ol.style.Style({
-        image: new ol.style.Circle({radius: 4.0 + size,
-             fill: new ol.style.Fill({color: 'rgba(255,5,1,1.0)'})}),
+        stroke: new ol.style.Stroke({color: 'rgba(255,1,1,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
